204125c08f073e64cae9e1f26728dd326a001446
khalidreda <khalidaziz2198@gmail.com>
2021-12-20 13:19:36 +0200

    More on Variables

---
+++ 3-JavaScript Basics Variables and Flow Control/js-bootcamp/basics/variables.js
@@ -0,0 +1,11 @@
+// You can't define a variable more than once
+let petName = "Hal";
+// let petName = "Spot";
+petName="Spot"
+console.log(petName)
+// There are rules related to variable names 
+// let 3 = 2;
+// let result = 3 + 4;
+// let test_ = 2;
+// variable names cannot be reserved keywords
+// let llet = 12;
