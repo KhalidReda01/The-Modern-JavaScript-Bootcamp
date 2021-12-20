4a157f841d204e3a7de5395301956063b88e7763
khalidreda <khalidaziz2198@gmail.com>
2021-12-20 15:52:55 +0200

    If statments

--- 3-JavaScript Basics Variables and Flow Control/js-bootcamp/basics/boolean.js
+++ 3-JavaScript Basics Variables and Flow Control/js-bootcamp/basics/boolean.js
@@ -1,4 +1,4 @@
-let temp = 32;
+let temp =180;
 // === - equality operator
 // !== - no equal operator
 // < - less than operator
@@ -7,9 +7,17 @@ let temp = 32;
 // >= - greater than or equal to opeator 
 // let isFreezing = temp !== 32;
 // let isFreezing = temp === 31;
-let isFreezing = temp <=32;
-console.log(isFreezing)
+// let isFreezing = temp <=32;
+// console.log(isFreezing)
 
+
+if (temp <=32) {
+  console.log("It is freezing outside")
+}
+if (temp >= 110) {
+  console.log("It's too hot outside");
+  console.log("Testing123")
+}
 // Challenge area
 // create age set to your age
 let age =30;
@@ -28,9 +36,13 @@ if (age < 7) {
 // calculate is senior - if the are 65 or older
 // print is child value
 // print is senior value
+
+//if or under print message about child pricing
+// if 65 or older print message about senior discount
+// I already did that at the last lecture
 console.log("You are a age is " + age + " you are a " + calculate + " You deserve a discount")
 // I used if else instead much better 
-let isChild = age <= 7;
-let isSenior = age >= 65;
-console.log(isChild)
-console.log(isSenior)
+// let isChild = age <= 7;
+// let isSenior = age >= 65;
+// console.log(isChild)
+// console.log(isSenior)
