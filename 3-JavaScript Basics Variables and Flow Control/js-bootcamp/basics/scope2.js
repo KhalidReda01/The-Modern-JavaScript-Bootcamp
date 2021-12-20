cdb7fcb38859db86f3452c46fcac39c170fd1726
khalidreda <khalidaziz2198@gmail.com>
2021-12-20 19:03:05 +0200

    variable scope:part2 End Section 3

---
+++ 3-JavaScript Basics Variables and Flow Control/js-bootcamp/basics/scope2.js
@@ -0,0 +1,18 @@
+//Global(name)
+  //Local(name)
+    //Local
+    //Local
+
+let name = "Khalid";
+
+if (true) {
+  let name = "Yousef"
+  if (true) {
+  // let  name="Reda"
+     console.log(name)
+  }
+}
+
+if (true) {
+  console.log(name)
+}
