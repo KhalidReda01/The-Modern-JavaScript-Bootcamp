39dffac437e0d569d0a1c33e4587bd2aeca0a4d5
khalidreda <khalidaziz2198@gmail.com>
2021-12-20 13:52:13 +0200

    Build a Temperature Converter

---
+++ 3-JavaScript Basics Variables and Flow Control/js-bootcamp/basics/temp-conversion.js
@@ -0,0 +1,14 @@
+let fahrenheit = 40;// //0c 273.15 k
+// calculate celsius and store in celsisu varialbe
+//T(°C) = (T(°F) - 32) × 5/9
+
+let celsius = (fahrenheit - 32) * 5 / 9;
+
+// print that value
+console.log(celsius)
+// calculate kelvin value and store in variable
+//T(K) = T(°C) + 273.15
+
+let kelvin = celsius + 273.15;
+//print that value
+console.log(kelvin)
