0da28fd0755220d21b8d5256867ea73f95ccf0e4
khalidreda <khalidaziz2198@gmail.com>
2021-12-20 11:38:12 +0200

    Strings and Variables

---
+++ 3-JavaScript Basics Variables and Flow Control/js-bootcamp/basics/strings.js
@@ -0,0 +1,11 @@
+// let name = "Khalid Reda";
+// console.log(name)
+// let firstName = "Khalid";
+// let lastName = "Reda";
+// let fullName = firstName + " " + lastName;
+// console.log(fullName)
+// challenge 1 city, country, location 
+let city = "Mansura";
+let country = "Egypt";
+let location = city + ", " + country;
+console.log(location)
