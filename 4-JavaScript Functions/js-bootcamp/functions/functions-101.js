06eb142169899882b36291e8342bba6d27ebb89e
khalidreda <khalidaziz2198@gmail.com>
2021-12-21 11:12:32 +0200

    Functions Basics

---
+++ 4-JavaScript Functions/js-bootcamp/functions/functions-101.js
@@ -0,0 +1,21 @@
+//Function - input (argument) , code, output (return value)
+let greetUser = function () {
+  console.log('Welcome user!')
+}
+// greetUser();
+// greetUser();
+// greetUser();
+
+let square = function (num) {
+  let result = num * num
+  return result
+  // console.log(num)
+}
+
+
+let value = square(3)
+let otherValue = square(10)
+console.log(value)
+console.log(otherValue)
+
+
