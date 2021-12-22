acd529492f4e90f6bd49b51cec580482324ab86a
khalidreda <khalidaziz2198@gmail.com>
2021-12-22 21:05:11 +0200

    Undefined and Null

---
+++ 4-JavaScript Functions/js-bootcamp/functions/undefined-null.js
@@ -0,0 +1,23 @@
+// Undefined for variable 
+let name;
+// let name = "Khalid";
+name = "Khalid"
+if (name===undefined) {
+  console.log("Please provide a name")
+} else {
+  console.log(name)
+}
+// console.log(name)
+// undefined for function arguments 
+// Undefiend as funciton return defalut value 
+let square = function(num){
+console.log(num)
+}
+// square()
+let result = square()
+console.log(result)
+// NUll as asigned value 
+let age = 23;
+// age = undefined;
+age = null
+console.log(age)
