24c0cb4d17f879365f4918a1fdcaebf8741686e2
khalidreda <khalidaziz2198@gmail.com>
2021-12-24 10:45:02 +0200

    Build a Grade Calculator

--- 4-JavaScript Functions/js-bootcamp/functions/grade-calc.js
+++ 4-JavaScript Functions/js-bootcamp/functions/grade-calc.js
@@ -12,22 +12,36 @@ let grade;
 console.log(`${percent}%`)
   if (percent >= 90 && percent <= 100) {
   grade="A"
-    return `You got a ${grade} grade (${percent}%)`
+    // return `You got a ${grade} grade (${percent}%)`
   } else if (percent >= 80 && percent <= 89) {
     grade="B"
-    return `You got a ${grade} grade (${percent}%)`
+    // return `You got a ${grade} grade (${percent}%)`
 } else if (percent >= 78 && percent <= 79) {
   grade="C"
-  return `You got a ${grade} grade (${percent}%)`
+  // return `You got a ${grade} grade (${percent}%)`
 } else if (percent >= 60 && percent <= 69) {
   grade="D"
-  return `You got a ${grade} grade (${percent}%)`
+  // return `You got a ${grade} grade (${percent}%)`
 } else if (percent >= 0 && percent <= 59) {
   grade="F"
-  return `You got a ${grade} grade (${percent}%)`
+  // return `You got a ${grade} grade (${percent}%)`
   } else {
     return `Please inter the correct score with the right formula`
-}
+  }
+  return `You got a ${grade} grade (${percent}%)`
 }
 let result = gradeCalc(20, 100)
-console.log(result )
+console.log(result)
+
+
+//Instructor Solution is the exact same as mine so no need to repeat writing his solution but you can ofcourse code along and practice again
+// let gradeCalcI = function (score, totalScore) {
+//   let percent = (score / totalScore) * 100
+//   if (percent >= 90) {
+//     return `You got a A (${percent})`
+//   }
+// }
+// let resultI = gradeCalcI(90, 100)
+// console.log(resultI)
+// like that and so on
+// hey no need to repeat the return statement every time like above Even Andrea said that if he write this 5 times again he will come up with different code but with the same output and you did that and that's totally okay
