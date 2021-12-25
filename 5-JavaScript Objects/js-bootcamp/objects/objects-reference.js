e5a4237f4a4de67b2d3979dec1079687df620240
khalidreda <khalidaziz2198@gmail.com>
2021-12-25 17:33:59 +0200

    Build an Expense Tracker

--- 5-JavaScript Objects/js-bootcamp/objects/objects-reference.js
+++ 5-JavaScript Objects/js-bootcamp/objects/objects-reference.js
@@ -3,7 +3,11 @@ let myAccount = {
   expenses: 0,
   income:0
 }
-
+let otherAccount = {
+  name: "Mohamed Reda",
+  expenses: 0,
+  income:0
+}
 
 // let otherAccount = myAccount
 // otherAccount.income=1000
@@ -11,10 +15,59 @@ let myAccount = {
 // otherAccount={}
 
 let addExpense = function (account, amount) {
-  account={}
-  // account.expenses = account.expenses + amount
+  account.expenses = account.expenses + amount
   console.log(account)
 }
-addExpense(myAccount, 2.50)
-console.log(myAccount.expenses)
-console.log(myAccount)
+// addExpense(myAccount, 2.50)
+// console.log(myAccount.expenses)
+// console.log(myAccount)
+
+
+// add Income
+let addIncome = function (account,amount) {
+  // account.income = account.income + amount
+  // console.log("Test this function "+account.income)
+  return account.income = account.income + amount
+}
+
+// resetAccount
+let resetAccount = function (account) {
+   account.name = "",  account.expenses = 0,  account.income = 0;
+}
+
+// getAccountSummary
+let getAccountSummary = function (account) {
+//  return  `Account for ${account.name} has ${account.income-account.expenses} income ${account.income}`
+console.log(`Account for ${account.name} has ${account.income-account.expenses} income ${account.income} expenses ${account.expenses}`) 
+}
+// Account for Andrew as the instructor said  has 900$. 1000% income 100$ expenses.
+// add Income
+// console.log(addIncome(myAccount,1000))
+// addIncome()
+// add Expense
+// get Account summary
+// reset Account
+// get Account summary
+// console.log(getAccountSummary(myAccount))
+
+let result = getAccountSummary(myAccount)
+// console.log(result)
+//reset Account
+resetAccount(myAccount)
+// console.log(myAccount)
+
+
+// Test otherAccount
+// first I will print the object
+console.log(otherAccount)// work fine { name: 'Mohamed Reda', expenses: 0, income: 0 }
+//add expense 
+addExpense(otherAccount, 50) // print { name: 'Mohamed Reda', expenses: 50, income: 0 }
+addIncome(otherAccount, 1000)// print 
+console.log(otherAccount) 
+getAccountSummary(otherAccount)//Account for Mohamed Reda has 950 income 1000 expenses 50
+//reset again
+resetAccount(otherAccount)
+getAccountSummary(otherAccount)
+// print the account after reset 
+console.log(otherAccount)
+
