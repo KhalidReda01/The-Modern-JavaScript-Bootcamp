19789dae38b899803f4aa37b0ab7c9214bc87082
khalidreda <khalidaziz2198@gmail.com>
2021-12-25 13:59:09 +0200

    Object Basics

---
+++ 5-JavaScript Objects/js-bootcamp/objects/objects-101.js
@@ -0,0 +1,10 @@
+let myBook = {
+  title: '1984',
+  author: "George Orwell",
+  pageCount:236
+}
+// console.log(myBook)
+// console.log(myBook.title)
+console.log(`${myBook.title} by ${myBook.author}`)
+myBook.title = 'Animal Farm'
+console.log(`${myBook.title} by ${myBook.author}`)
