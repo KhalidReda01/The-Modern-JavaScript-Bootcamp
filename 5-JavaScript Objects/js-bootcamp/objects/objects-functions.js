c9c1b08a40da3806d0c2dcb32270ef809d32a433
khalidreda <khalidaziz2198@gmail.com>
2021-12-25 14:43:26 +0200

    Challenge Area

--- 5-JavaScript Objects/js-bootcamp/objects/objects-functions.js
+++ 5-JavaScript Objects/js-bootcamp/objects/objects-functions.js
@@ -14,8 +14,37 @@ let getSummary = function (book) {
     summary: `${book.title} by ${book.author}`,
     pageCountSummary:`${book.title} is ${book.pageCount} pages  long`
   }
-  console.log(`${book.title} by ${book.author}`)
+  // console.log(`${book.title} by ${book.author}`)
 }
 let bookSummary=getSummary(myBook)
 let otherBookSummary = getSummary(otherBook)
 console.log(bookSummary.pageCountSummary)
+// Challenge Area
+// Challenge Area on functions-101.js
+// Create function - take fahrenheit in -return object with all three 
+
+let convertFahrenheitToCelsius = function (fahrenheit) {
+  let celsius = (fahrenheit - 32) * 5 / 9
+let kelvin = (fahrenheit + 459.67) * 5 / 9
+  return  {
+    fahrenheit: `The Fahrenheit degree is ${fahrenheit}`,
+    celsius: `The celsius degree is ${celsius}`,
+      kelvin:`The  kelvin degree is ${kelvin}`
+
+  }
+}
+
+// let tempOne = convertFahrenheitToCelsius(32)
+let tempOne = convertFahrenheitToCelsius(20)
+// let tempTwo = convertFahrenheitToCelsius(68)
+
+console.log(tempOne.celsius)
+console.log(tempOne.kelvin)
+// console.log(tempTwo)
+//temp-conversion.js
+// let fahrenheit = 50 // 0c - 273.15k
+// let celsius = (fahrenheit - 32) * 5 / 9
+// let kelvin = (fahrenheit + 459.67) * 5 / 9
+
+// console.log(celsius)
+// console.log(kelvin)
