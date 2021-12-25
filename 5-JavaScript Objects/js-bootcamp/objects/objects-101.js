2f599eb7180beebea50761351d637188c97ee796
khalidreda <khalidaziz2198@gmail.com>
2021-12-25 14:11:35 +0200

    Challeng Area

--- 5-JavaScript Objects/js-bootcamp/objects/objects-101.js
+++ 5-JavaScript Objects/js-bootcamp/objects/objects-101.js
@@ -8,3 +8,15 @@ let myBook = {
 console.log(`${myBook.title} by ${myBook.author}`)
 myBook.title = 'Animal Farm'
 console.log(`${myBook.title} by ${myBook.author}`)
+// Challenge Area
+// name , age , location
+// Khalid is 23 and lives in  Mansura
+// Increse age by 1 and print message again 
+let myId = {
+  name: "Khalid",
+  age: 23,
+  location:"Mansura"
+}
+console.log(`${myId.name} is ${myId.age} lives in ${myId.location}`)
+  myId.age = 22;
+  console.log(`${myId.name} is ${myId.age} lives in ${myId.location}`)
