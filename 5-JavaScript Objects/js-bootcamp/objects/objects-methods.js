c671a83760118c6978206417f5938d4e81185eb4
khalidreda <khalidaziz2198@gmail.com>
2021-12-26 21:47:45 +0200

    Challenge Area

--- 5-JavaScript Objects/js-bootcamp/objects/objects-methods.js
+++ 5-JavaScript Objects/js-bootcamp/objects/objects-methods.js
@@ -12,10 +12,25 @@ let seatsLeft= this.guestCapcity-this.guestCount
     console.log(seatsLeft)
     return partySize <= seatsLeft
   },
- 
+  //Challenge Area
+  seatParty: function (seatsParty) {
+   let addParty= this.guestCount = this.guestCount + seatsParty
+    return addParty
+  },
+  removeParty: function (removeNumber) {
+    let removeParty = this.guestCount = this.guestCount -removeNumber
+    return removeParty
+  }
 }
 // seatParty
 // removeParty
-let status = restaurant.checkAvailablity(50)
-
-console.log(status)
+// let status = restaurant.checkAvailablity(50)
+// let test = restaurant.seatParty(20)
+// let removeReslut = restaurant.seatParty(50)
+// console.log(removeReslut)
+// console.log(test)
+// console.log(status)
+console.log(restaurant.seatParty(74))
+console.log(restaurant.checkAvailablity(4))
+console.log(restaurant.removeParty(5))
+console.log(restaurant.checkAvailablity(4))
