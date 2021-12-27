2e490ca2969eaca26815b80b19e1dad442771a6c
khalidreda <khalidaziz2198@gmail.com>
2021-12-27 16:12:08 +0200

    Challenge Area number-methods.js

--- 5-JavaScript Objects/js-bootcamp/objects/number-methods.js
+++ 5-JavaScript Objects/js-bootcamp/objects/number-methods.js
@@ -11,6 +11,14 @@ let max = 1;
 // to get that done we use a forumula math.floor(Math.randomw() *(max-min +1))+min
 let randomNum =Math.floor(Math.random()*(max-min +1)) 
 console.log(randomNum)
-// Test this formula 
+// Test this formula
 // console.log(Math.floor(Math.random() * (12 - 10) + 10))
 // Math.floor(Math.random() * (max - min + 1) + min)
+// Challenge Area
+// 1-5 -true if correct - false if not correct
+let makeGuess = function (guessNum) {
+  let randomNumbers = Math.floor(Math.random() * (5 - 1 + 1) + 1)
+  console .log(`your guessed Number is ${guessNum} and the random Number is ${randomNumbers}`)
+return randomNumbers ===guessNum
+}
+console.log(makeGuess(5))
