73e9f06bbd63189e137561a3907561b39c87f043
khalidreda <khalidaziz2198@gmail.com>
2021-12-27 14:09:08 +0200

    Exploring String Methods

---
+++ 5-JavaScript Objects/js-bootcamp/objects/string-methods.js
@@ -0,0 +1,16 @@
+let name = "     Khalid Reda";
+// Length Property
+console.log(name.length);
+// Convert to upper case 
+
+console.log(name.toUpperCase())
+
+// Convert to lowe case
+console.log(name.toLowerCase())
+// Includes method
+let password = "abc123password098"
+// console.log(password.includes('password',7))
+console.log(password.includes('password'))
+// Trim
+console.log(name)
+console.log(name.trim())
