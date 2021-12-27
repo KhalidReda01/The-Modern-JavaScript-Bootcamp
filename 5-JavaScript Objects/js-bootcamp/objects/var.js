5e1e80ba0a32d5e33e1e5b5e53218173afb6f134
khalidreda <khalidaziz2198@gmail.com>
2021-12-27 17:14:24 +0200

    Bonus: Variables with var

---
+++ 5-JavaScript Objects/js-bootcamp/objects/var.js
@@ -0,0 +1,17 @@
+var firstName = 'Khalid';
+// firstName = "Moha"
+if (10 === 10) {
+  let firstName= "Reda"
+//  console.log(firstName)
+}
+const setName = function () {
+  var firstName = "reda"
+}
+setName()
+  console.log(firstName)
+// let age;
+age =10;
+console.log(age)
+var age 
+// that's why we use let const
+// console.log(age)
