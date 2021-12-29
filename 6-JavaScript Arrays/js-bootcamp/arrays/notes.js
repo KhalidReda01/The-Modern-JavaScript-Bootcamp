8e226437776691fa35822c5bbfe9d7e21501fcce
khalidreda <khalidaziz2198@gmail.com>
2021-12-29 16:55:27 +0200

    Array Basics

---
+++ 6-JavaScript Arrays/js-bootcamp/arrays/notes.js
@@ -0,0 +1,6 @@
+const notes = ['Note 1', 'Note 2', 'Note 3']
+console.log(notes.length)
+console.log(notes[0])
+console.log(notes[2])
+console.log(notes[3])
+console.log(notes[notes.length-1])
---
+++ 6-JavaScript Arrays/js-bootcamp/arrays/todo.js
@@ -0,0 +1,3 @@
+// Create an array with five todas
+// You have x todos 
+// Print the first and second to last items = > Todo : walk the dog 
