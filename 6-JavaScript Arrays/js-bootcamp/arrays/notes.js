cb48303afb535dec1c6c9ae420d6cb41eedf0c4f
khalidreda <khalidaziz2198@gmail.com>
2021-12-29 19:33:49 +0200

    Looping over Arrays

--- 6-JavaScript Arrays/js-bootcamp/arrays/notes.js
+++ 6-JavaScript Arrays/js-bootcamp/arrays/notes.js
@@ -1,5 +1,5 @@
 const notes = ['Note 1', 'Note 2', 'Note 3']
-// // remove from the end 
+// // remove from the end
 // console.log(notes.pop())
 // // add to the end
 // notes.push('MY New note')
@@ -12,10 +12,19 @@ const notes = ['Note 1', 'Note 2', 'Note 3']
 // console.log(notes.shift())
 // console.log(notes)
 // console.log(notes.unshift("add new note"))
-console.log(notes)
-console.log(notes.splice(0 , 1 ,"This is the new Item  using Splice"))
-console.log(notes)
-notes[2] = "hello test"
-console.log(notes)
+// console.log(notes)
+// console.log(notes.splice(0 , 1 ,"This is the new Item  using Splice"))
+// console.log(notes)
+// notes[2] = "hello test"
+// console.log(notes)
 
 
+//Looping over Arrays
+// const doThis = function () {
+  
+// }
+// notes.forEach(doThis)
+notes.forEach(function (item, index) {
+  console.log(index)
+  console.log(item)
+})
