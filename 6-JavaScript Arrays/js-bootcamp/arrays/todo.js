4dc484b60a3401e3f26f87954b81e5bf7630816f
khalidreda <khalidaziz2198@gmail.com>
2021-12-29 17:05:31 +0200

    Challenge Area

--- 6-JavaScript Arrays/js-bootcamp/arrays/todo.js
+++ 6-JavaScript Arrays/js-bootcamp/arrays/todo.js
@@ -1,3 +1,7 @@
 // Create an array with five todas
+const toDo = ['Do your homwork','make  your dinner ','help  your mom','adjust your desk/workspace']
 // You have x todos 
+console.log(`You have ${toDo.length} todos`)
 // Print the first and second to last items = > Todo : walk the dog 
+console.log(toDo[0])
+console.log(toDo[3])// toDo[toDo.lenght-1]
