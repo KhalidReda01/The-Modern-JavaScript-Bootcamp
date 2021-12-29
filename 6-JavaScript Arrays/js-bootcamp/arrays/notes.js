554c4ae97f2e85ec6eea052645d7b58a6f9b147c
khalidreda <khalidaziz2198@gmail.com>
2021-12-29 18:19:23 +0200

    Manipulating Arrays with Methods

--- 6-JavaScript Arrays/js-bootcamp/arrays/notes.js
+++ 6-JavaScript Arrays/js-bootcamp/arrays/notes.js
@@ -1,6 +1,21 @@
 const notes = ['Note 1', 'Note 2', 'Note 3']
-console.log(notes.length)
-console.log(notes[0])
-console.log(notes[2])
-console.log(notes[3])
-console.log(notes[notes.length-1])
+// // remove from the end 
+// console.log(notes.pop())
+// // add to the end
+// notes.push('MY New note')
+// console.log(notes.length)
+// console.log(notes[0])
+// console.log(notes[2])
+// console.log(notes[3])
+// console.log(notes[notes.length - 1])
+// console.log(notes)
+// console.log(notes.shift())
+// console.log(notes)
+// console.log(notes.unshift("add new note"))
+console.log(notes)
+console.log(notes.splice(0 , 1 ,"This is the new Item  using Splice"))
+console.log(notes)
+notes[2] = "hello test"
+console.log(notes)
+
+
