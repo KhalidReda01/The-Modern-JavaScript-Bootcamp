d260a73ffe7d964c02ccc6646e97ca9527d9cbf3
khalidreda <khalidaziz2198@gmail.com>
2021-12-29 19:46:53 +0200

    Challenge Area Looping over Arrays

--- 6-JavaScript Arrays/js-bootcamp/arrays/todo.js
+++ 6-JavaScript Arrays/js-bootcamp/arrays/todo.js
@@ -1,25 +1,37 @@
 // Create an array with five todas
 const toDo = ['Do your homwork','make  your dinner ','help  your mom','adjust your desk/workspace']
-// You have x todos 
+// You have x todos
 // console.log(`You have ${toDo.length} todos`)
 // Print the first and second to last items = > Todo : walk the dog
 // console.log(toDo[0])
 // console.log(toDo[3])// toDo[toDo.lenght-1]
 // Challengre Area
 // Delete the 3rd Items
-console.log(toDo)
-// Add a new item onto the end
-console.log(toDo.length)
+// console.log(toDo)
+// // Add a new item onto the end
+// console.log(toDo.length)
 
-toDo.push("Eat your lunch")
-console.log(toDo)
-console.log(toDo.length)
-// Remove the first item from the list
-// we can use shift and splice
-console.log(toDo)
-console.log(toDo[0])
+// toDo.push("Eat your lunch")
+// console.log(toDo)
+// console.log(toDo.length)
+// // Remove the first item from the list
+// // we can use shift and splice
+// console.log(toDo)
+// console.log(toDo[0])
+// toDo.shift()
+// console.log(toDo[0])
+// toDo.splice(0, 1)
+// console.log(toDo[0])
+// console.log(toDo)
+// challengre Area
+toDo.splice(2, 1)
+toDo.push("Buy Coffee")
 toDo.shift()
-console.log(toDo[0])
-toDo.splice(0, 1)
-console.log(toDo[0])
 console.log(toDo)
+//1.The first Item
+//2.The second Items
+toDo.forEach(function (item, index) {
+  console.log(`${index+1}. ${item}`)
+  // console.log(`ToDo index is ${index} ToDo Item is ${item}`)
+  // console.log(item)
+})
