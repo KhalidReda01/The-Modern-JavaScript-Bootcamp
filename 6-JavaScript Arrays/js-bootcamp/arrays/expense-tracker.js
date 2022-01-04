8eabf12a7e5682c602f0a62b2f81115ff2210c91
khalidreda <khalidaziz2198@gmail.com>
2022-01-04 23:58:58 +0200

    Challengre Area expense-tracker print the same output done

--- 6-JavaScript Arrays/js-bootcamp/arrays/expense-tracker.js
+++ 6-JavaScript Arrays/js-bootcamp/arrays/expense-tracker.js
@@ -1,8 +1,23 @@
 const account = {
   name: "Khalid Reda",
-  expenses:[]
+  expenses: [],
+  addExpense: function (expenses, value) {
+    
+    // this.expenses= Number(this.expenses) + Number(value);
+    // console.log(typeof this.expenses + value )
+    return this.expenses= Number(this.expenses) + Number(value)
+  },
+  getAccountSummary: function () {
+    // console.log(`${this.name} has $ ${this.expenses} in expenses`)
+    return `${this.name} has $ ${this.expenses} in expenses`
+  }
 }
 // Expense  -> description , amount
+// const addExpense = function (expenses,value) {
+//   account.expenses = value;
+// }
+// addExpense('Rent', 950)
+// console.log(account)
 // addExpense -> description , amount
 // getAccountSummary -> total up all expenses -> Khalid Reda has $ 500 in expenses if you want to calculate the total use forEach 
 account.addExpense('Rent', 950)
