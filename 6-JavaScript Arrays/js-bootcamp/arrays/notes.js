036ce32da5390c6a21f97f087f4d6f0150cfd1fc
khalidreda <khalidaziz2198@gmail.com>
2022-01-04 23:04:35 +0200

    Sorting Arrays

--- 6-JavaScript Arrays/js-bootcamp/arrays/notes.js
+++ 6-JavaScript Arrays/js-bootcamp/arrays/notes.js
@@ -1,42 +1,59 @@
 
-const notes = [
- {
-  title: "My next Trip",
-  body:"I would like to go to Turkey"
-}, {
-  title: "Habbits to work on",
-  body:"Exercise. Eating a bit better"
-  }, {
-  title: "Office Modification",
-  body:"Get a new seat"
-  }]
-// const findNote = function (notes, noteTitle) {
-//   const index = notes.findIndex(function (note, index) {
+// // const findNote = function (notes, noteTitle) {
+// //   const index = notes.findIndex(function (note, index) {
    
-//     return note.title.toLowerCase() === noteTitle.toLowerCase() // it gives me the same message // to just keep going I will not use the toLowerCase until I find a solution because I don't see why toLowerCase is not defind
-//   })
-//   return notes[index]
-// }
-// const findNote = function (notes, noteTitle) {
-//  return  notes.find(function (note, index) {
-//        return note.title.toLowerCase() === noteTitle.toLowerCase() // it gives me the same message // to just keep going I will not use the toLowerCase until I find a solution because I don't see why toLowerCase is not defind
-//   })
-// }
+// //     return note.title.toLowerCase() === noteTitle.toLowerCase() // it gives me the same message // to just keep going I will not use the toLowerCase until I find a solution because I don't see why toLowerCase is not defind
+// //   })
+// //   return notes[index]
+// // }
+// // const findNote = function (notes, noteTitle) {
+// //  return  notes.find(function (note, index) {
+// //        return note.title.toLowerCase() === noteTitle.toLowerCase() // it gives me the same message // to just keep going I will not use the toLowerCase until I find a solution because I don't see why toLowerCase is not defind
+// //   })
+// // }
 
-// Trip , ne flexible searching
-// explore this techinpe outside of function
-const findNotes = function (notes,query) {
- return  notes.filter(function (note, index) {
-  const isTitleMatch = note.title.toLowerCase().includes(query.toLowerCase())
-  const isBodyMatch = note.body.toLowerCase().includes(query.toLowerCase())
-  return isTitleMatch || isBodyMatch
-  })
+// // Trip , ne flexible searching
+// // explore this techinpe outside of function
+// const findNotes = function (notes,query) {
+//  return  notes.filter(function (note, index) {
+//   const isTitleMatch = note.title.toLowerCase().includes(query.toLowerCase())
+//   const isBodyMatch = note.body.toLowerCase().includes(query.toLowerCase())
+//   return isTitleMatch || isBodyMatch
+//   })
  
-}
+// }
 
-console.log(findNotes(notes,"office"))
-  //  console.log(filteredNotes)
+// console.log(findNotes(notes,"office"))
+//   //  console.log(filteredNotes)
 
 
-// const note = findNote(notes, "Habbits to work on")
-// console.log(note)
+// // const note = findNote(notes, "Habbits to work on")
+// // console.log(note)
+// 44.Sorting Arrays
+// console.log('a'<'A')
+const notes = [
+  {
+   title: "My next Trip",
+   body:"I would like to go to Turkey"
+ }, {
+   title: "Habbits to work on",
+   body:"Exercise. Eating a bit better"
+   }, {
+   title: "Office Modification",
+   body:"Get a new seat"
+  }]
+const sortNotes = function (notes) {
+  notes.sort(function (a,b) {
+    if (a.title.toLowerCase() <b.title.toLowerCase()) {
+        return -1
+    } else if (b.title.toLowerCase() <a.title.toLowerCase()) {
+      return 1
+    } else {
+      return 0
+      }
+    })
+  }
+// console.log(notes)
+sortNotes(notes)
+
+console.log(notes)
--- 6-JavaScript Arrays/js-bootcamp/arrays/todo.js
+++ 6-JavaScript Arrays/js-bootcamp/arrays/todo.js
@@ -1,3 +1,25 @@
+// // const deleteTodo = function (todos, todoText) {
+// //   const index = todos.findIndex(function (todo){
+// //     return todo.text.toLowerCase() ===todoText.toLowerCase()
+// //   })
+// //   if (index > -1) {
+// //     todos.splice(index,1)
+// //   }
+// // }
+
+// // deleteTodo(toDoo, 'Do your homwork')
+// // console.log(toDoo)
+// //Challenge Area
+// // create a funciton that use filter to get a list of todoos that have completed vlue of false that will be help your mom and ajdust your desktop
+// const getThingsToDo = function (toDoo) {
+//   const result = toDoo.filter(function (todo, index) {
+//     const isCompletedFalse = todo.completed === false;
+//     return isCompletedFalse
+//   })
+//   return result
+// }
+// console.log(getThingsToDo(toDoo))
+// // Cool solved with no time that's why I said you have to watch the lecture again before actually doing the challenge it that's amazing 
 const toDoo = [
   {
     text: 'Do your homwork', completed: true
@@ -8,26 +30,3 @@ const toDoo = [
   }, {
     text: 'adjust your desk/workspace', completed: false
   }]
-
-// const deleteTodo = function (todos, todoText) {
-//   const index = todos.findIndex(function (todo){
-//     return todo.text.toLowerCase() ===todoText.toLowerCase()
-//   })
-//   if (index > -1) {
-//     todos.splice(index,1)
-//   }
-// }
-
-// deleteTodo(toDoo, 'Do your homwork')
-// console.log(toDoo)
-//Challenge Area
-// create a funciton that use filter to get a list of todoos that have completed vlue of false that will be help your mom and ajdust your desktop
-const getThingsToDo = function (toDoo) {
-  const result = toDoo.filter(function (todo, index) {
-    const isCompletedFalse = todo.completed === false;
-    return isCompletedFalse
-  })
-  return result
-}
-console.log(getThingsToDo(toDoo))
-// Cool solved with no time that's why I said you have to watch the lecture again before actually doing the challenge it that's amazing 
