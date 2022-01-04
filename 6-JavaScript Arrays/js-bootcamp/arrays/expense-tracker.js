ea67d9e29cf179598ef2be9e2864ad8a64fec359
khalidreda <khalidaziz2198@gmail.com>
2022-01-05 00:21:03 +0200

    Challengre Are Instructor Guide

--- 6-JavaScript Arrays/js-bootcamp/arrays/expense-tracker.js
+++ 6-JavaScript Arrays/js-bootcamp/arrays/expense-tracker.js
@@ -5,11 +5,24 @@ const account = {
     
     // this.expenses= Number(this.expenses) + Number(value);
     // console.log(typeof this.expenses + value )
-    return this.expenses= Number(this.expenses) + Number(value)
+    // return this.expenses= Number(this.expenses) + Number(value)
+    // instructor solution
+    // haha I thout that he wanted it like that but I just print the output as he wanted
+    this.expenses.push({
+      description: expenses,
+      amount:value
+    })
   },
   getAccountSummary: function () {
+    let totalExpenses = 0;
+    this.expenses.forEach(function (expense) {
+      totalExpenses= totalExpenses+expense.amount
+      
+    })
+    return `${this.name} has $ ${totalExpenses} in expenses.`
     // console.log(`${this.name} has $ ${this.expenses} in expenses`)
-    return `${this.name} has $ ${this.expenses} in expenses`
+    // console.log(this.expenses.amount)
+    // return `${this.name} has $ ${this.expenses} in expenses`
   }
 }
 // Expense  -> description , amount
@@ -19,7 +32,10 @@ const account = {
 // addExpense('Rent', 950)
 // console.log(account)
 // addExpense -> description , amount
-// getAccountSummary -> total up all expenses -> Khalid Reda has $ 500 in expenses if you want to calculate the total use forEach 
+// getAccountSummary -> total up all expenses -> Khalid Reda has $ 500 in expenses if you want to calculate the total use forEach
+
 account.addExpense('Rent', 950)
 account.addExpense('Coffe', 2)
+console.log(account)
 console.log(account.getAccountSummary()) // print Khalid Reda has 952 $ in expenses 
+// console.log(account.expenses.description) // It's not working like that It's an array of objects 
