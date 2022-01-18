999588ab937da8df5a26927ed927066290bf26d6
khalidreda <khalidaziz2198@gmail.com>
2022-01-18 22:06:43 +0200

    DOM Challenge 2nd

--- 7-JacaScirpt into the browser/Js-Bootcamp/todo-app/app.js
+++ 7-JacaScirpt into the browser/Js-Bootcamp/todo-app/app.js
@@ -1,6 +1,24 @@
 /**
- * 
- * 
- * 
- * 
- */
+ * Second Challenge
+ * 1. Create and link a Jacascript file to HTML
+ * 2. Remove all p that have "The" in the text 
+ *  
+//  */
+// const todos = document.querySelectorAll('p')
+// // console.log(todos[3].textContent.search('the'))
+// todos.forEach(function (todo) {
+//   // todo.includes("the")
+//   // const findThe = todo.search('the')
+//   // console.log(findThe)
+   
+  
+// })
+ 
+const paragraphs = document.querySelectorAll('p')
+paragraphs.forEach(function (paragraph) {
+  // console.log(paragraph.textContent.includes('the'))
+  if (paragraph.textContent.includes('the')) {
+    // The issue was that you missed the textContent and just have the paragraph.includes is not a function
+    paragraph.remove()
+  }
+})
