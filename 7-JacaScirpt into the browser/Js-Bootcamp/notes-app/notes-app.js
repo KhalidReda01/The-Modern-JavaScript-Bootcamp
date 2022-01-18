3ed26ba048551192e68cbf06af21d64d1508ee99
khalidreda <khalidaziz2198@gmail.com>
2022-01-18 21:11:31 +0200

    DOM Manpulation

--- 7-JacaScirpt into the browser/Js-Bootcamp/notes-app/notes-app.js
+++ 7-JacaScirpt into the browser/Js-Bootcamp/notes-app/notes-app.js
@@ -1,1 +1,14 @@
 console.log("This is from my HTML document")
+// DOM Document Object Model 
+// Query and remove 
+const p = document.querySelector('p')
+// console.log(p)
+// p.remove()
+// Query all and remove 
+const ps = document.querySelectorAll('p')
+ps.forEach(function (p) {
+  p.textContent="******************* Replaced with Dom"
+  // p.remove()
+  // console.log(p)
+//  console.log(p.textContent)
+})
