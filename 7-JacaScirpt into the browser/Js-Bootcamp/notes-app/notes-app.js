a0e06708f0b72cde503782521749cbde6699818b
khalidreda <khalidaziz2198@gmail.com>
2022-01-19 13:53:08 +0200

    Handling User Interaction

--- 7-JacaScirpt into the browser/Js-Bootcamp/notes-app/notes-app.js
+++ 7-JacaScirpt into the browser/Js-Bootcamp/notes-app/notes-app.js
@@ -10,17 +10,23 @@ const notes = [
    body:"Get a new seat"
   }]
 
-console.log("This is from my HTML document")
-// DOM Document Object Model 
-// Query and remove 
-const p = document.querySelector('p')
-// console.log(p)
-// p.remove()
-// Query all and remove 
-const ps = document.querySelectorAll('p')
-ps.forEach(function (p) {
-  p.textContent="******************* Replaced with Dom"
-  // p.remove()
-  // console.log(p)
-//  console.log(p.textContent)
+// console.log("This is from my HTML document")
+// // DOM Document Object Model
+// // Query and remove
+// const p = document.querySelector('p')
+// // console.log(p)
+// // p.remove()
+// // Query all and remove
+// const ps = document.querySelectorAll('p')
+// ps.forEach(function (p) {
+//   p.textContent="******************* Replaced with Dom"
+//   // p.remove()
+//   // console.log(p)
+// //  console.log(p.textContent)
+// })
+document.querySelector('button').addEventListener('click', function (event) {
+  // console.log('Did this work? ')
+  // // console.log(event)
+  // // console.log(event.target)
+  event.target.textContent="The was clicked"
 })
