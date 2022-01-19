0adb1cfaad55569cf28fe07b4a66901bb26cb373
khalidreda <khalidaziz2198@gmail.com>
2022-01-19 11:28:20 +0200

    Adding Elements Via the Dom Solving Challenge Area

--- 7-JacaScirpt into the browser/Js-Bootcamp/todo-app/app.js
+++ 7-JacaScirpt into the browser/Js-Bootcamp/todo-app/app.js
@@ -10,19 +10,9 @@
 //   // todo.includes("the")
 //   // const findThe = todo.search('the')
 //   // console.log(findThe)
-   
-  
+
+
 // })
-const toDoo = [{text:'clearn your clothes',completed:false},
-{
-  text: 'Do your homwork', completed: true
-}, {
-  text: 'make  your dinner', completed: true
-}, {
-  text: 'help  your mom', completed: false
-}, {
-  text: 'adjust your desk/workspace', completed: false
-}]
 
 // const paragraphs = document.querySelectorAll('p')
 // paragraphs.forEach(function (paragraph) {
@@ -32,7 +22,46 @@ const toDoo = [{text:'clearn your clothes',completed:false},
 //     paragraph.remove()
 //   }
 // })
-// // Add  new element 
+// // Add  new element
 // const newParagraph = document.createElement('p')
 // newParagraph.textContent = 'This is a new element from JavaScript'
 // document.querySelector('body').appendChild(newParagraph)
+
+//Challengre Area
+const toDoo = [{
+    text: 'clearn your clothes',
+    completed: false
+  },
+  {
+    text: 'Do your homwork',
+    completed: true
+  }, {
+    text: 'make  your dinner',
+    completed: true
+  }, {
+    text: 'help  your mom',
+    completed: false
+  }, {
+    text: 'adjust your desk/workspace',
+    completed: false
+  }
+]
+//  You have  2 todos left this is the instructor not you
+//  You have 3 todos left (p element)
+// Add a p for each todo above (use text value )
+const createP = document.createElement('p')
+  createP.textContent = 'You have 3 todos left'
+document.querySelector('body').appendChild(createP)
+
+toDoo.forEach(function (todo) {
+  const createtodoP = document.createElement('p')
+
+  if (todo.completed === false) {
+    createtodoP.textContent = todo.text
+    document.querySelector('body').appendChild(createtodoP)
+  } else {
+    
+    // console.log('there is a problem here the problem was just you mistyped textContent you wrote textConent but you are the one who disover that ')
+  }
+ 
+})
