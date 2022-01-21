ec643cd0d443db82478506e77851b50f5039c173
khalidreda <khalidaziz2198@gmail.com>
2022-01-21 21:43:30 +0200

    Rendering our Filtered Data This lecture need SCMM

--- 7-JacaScirpt into the browser/Js-Bootcamp/notes-app/notes-app.js
+++ 7-JacaScirpt into the browser/Js-Bootcamp/notes-app/notes-app.js
@@ -24,6 +24,25 @@ const notes = [
 //   // console.log(p)
 // //  console.log(p.textContent)
 // })
+const filters = {
+  searchText:''
+}
+const renderNotes = function (notes, filters) {
+  const filteredNotes = notes.filter(function (note) {
+    return note.title.toLowerCase().includes(filters.searchText.toLowerCase())
+  })
+  console.log(filteredNotes)
+  // document.querySelector('#notes').innerHTML='<p>TEST</p>'
+  document.querySelector('#notes').innerHTML=''
+  filteredNotes.forEach(function (note) {
+    const noteEl = document.createElement("p")
+    noteEl.textContent = note.title
+    // document.querySelector('body').appendChild(noteEl)
+    document.querySelector('#notes').appendChild(noteEl)
+
+  })
+}
+renderNotes(notes,filters)
 document.querySelector('#create-note').addEventListener('click', function (event) {
   // console.log('Did this work? ')
   // // console.log(event)
@@ -37,8 +56,7 @@ document.querySelector('#remove-all').addEventListener('click', function () {
   })
 })
 document.querySelector('#search-text').addEventListener('input', function (e) {
-  console.log(e.target.value)
-  console.log(e.target)
-  console.log(e)
-
+  
+  filters.searchText = e.target.value
+  renderNotes(notes,filters)
 })
